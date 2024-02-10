23d02f5abdbe55868c5f7c86b2a1cbec1cf95687
Dmitry <dima2323fr3@gmail.com>
2024-02-10 11:20:39 +0700

    Сделал маленькую часть коструктора у класса VirMem

---
+++ LAB_1_METHODS_OF_PROG/VirtualMemory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_1_METHODS_OF_PROG
+{
+    public class VirtualMemory : IDisposable
+    {
+        protected FileStream? virtualFile { get; set; } = null;
+        protected Page[] pages;
+        protected string fileName=string.Empty;
+        protected long arraySize;
+
+        public VirtualMemory(string fileName, long arraySize)
+        {
+            this.fileName=fileName;
+            this.arraySize=arraySize;
+            if(!File.Exists(fileName)) 
+            {
+                virtualFile = new FileStream(fileName, FileMode.CreateNew, FileAccess.ReadWrite, FileShare.ReadWrite);
+                using StreamWriter stream = new StreamWriter(fileName);
+                stream.Write('V');
+                stream.Write('M');
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- LAB_1_METHODS_OF_PROG/obj/Debug/net8.0/LAB_1_METHODS_OF_PROG.AssemblyInfo.cs
+++ LAB_1_METHODS_OF_PROG/obj/Debug/net8.0/LAB_1_METHODS_OF_PROG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LAB_1_METHODS_OF_PROG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3088e24fddb71a1003aadfd8c52a1de8268fe17")]
 [assembly: System.Reflection.AssemblyProductAttribute("LAB_1_METHODS_OF_PROG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LAB_1_METHODS_OF_PROG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
