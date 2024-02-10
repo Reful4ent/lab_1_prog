e3088e24fddb71a1003aadfd8c52a1de8268fe17
rochaldo1 <xusainow.artur2018@yandex.ru>
2024-02-10 10:40:45 +0700

    структура страницы

---
+++ LAB_1_METHODS_OF_PROG/Page.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+namespace LAB_1_METHODS_OF_PROG
+{
+    public struct Page
+    {
+        public int _pageIndex;
+        public byte _pageMod;
+        public byte _recordInMemTime;
+        public BitArray _bitMap;
+        public int[] _valuesModelArray;
+    }
+}
